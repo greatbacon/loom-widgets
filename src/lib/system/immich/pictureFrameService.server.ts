50470fda823e1f5d6b19861f8bb86edb95a298ea
Hamilton R <hamiltonjreed@gmail.com>
2026-09-20 07:55:45 -0600

    more debugging

--- src/lib/system/immich/pictureFrameService.server.ts
+++ src/lib/system/immich/pictureFrameService.server.ts
@@ -56,11 +56,15 @@ export function computeDefaultCrop(
 }
 
 export function shouldCycleOnPull(now: Date = new Date()): boolean {
-	return now.getHours() === 1;
+	console.log('should we cycle?', now.getHours());
+	//return now.getHours() === 1;
+	return true;
 }
 
 export function computeNextCronTime(now: Date = new Date()): string {
-	const next = new Date(now.getTime() + 60 * 60 * 1000);
+	console.log('next pull time')
+	//const next = new Date(now.getTime() + (60 * 60 * 1000));
+	const next = new Date(now.getTime() + (10 * 60 * 1000));
 	return next.toISOString().replace(/\.\d{3}Z$/, 'Z');
 }
 
